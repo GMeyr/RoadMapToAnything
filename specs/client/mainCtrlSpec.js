4be82a388c7d27f03bcd36cfe50ed2165d851ed6
GMeyr <gabrielmeyr@gmail.com>
2016-01-22 16:47:46 -0500

    wokring test for mainCtrl

--- specs/client/mainCtrlSpec.js
+++ specs/client/mainCtrlSpec.js
@@ -1,15 +1,31 @@
 describe('MainController', function () {
+  var $scope, $rootScope, $location, createController, $httpBackend;
 
-  beforeEach(inject(function ($rootScope){
-    $scope = $rootScope.new();
+  // using angular mocks, we can inject the injector
+  // to retrieve our dependencies
+  beforeEach(module('app'));
+  beforeEach(inject(function($injector) {
+
+    // mock out our dependencies
+    $rootScope = $injector.get('$rootScope');
+    $httpBackend = $injector.get('$httpBackend');
+    $location = $injector.get('$location');
+
+    $scope = $rootScope.$new();
+
+    var $controller = $injector.get('$controller');
+
+    createController = function () {
+      return $controller('MainController', {
+        $scope: $scope
+      });
+    };
+
+    createController();
   }));
 
-  it('can be instantiated', inject(function($controller) {
-    var controller = $controller('MainController', {
-      $scope: $scope
-    });
-
-    expect(controller).not.toBeNull();
-  }));
+  it('should have a test method on the $scope', function () {
+    expect($scope.test).to.be.a('function');
+  });
 
 });
