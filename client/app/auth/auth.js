b27fca55ccf820976c198027bd5cb7664f31803f
GMeyr <gabrielmeyr@gmail.com>
2016-01-25 20:26:51 -0500

    build signup html page

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -2,6 +2,8 @@ angular.module('app.auth', [])
 
 .controller('AuthController', ['$scope', '$http', function($scope, $http){
 
+  $scope.attemptedFirstName = "not yet set";
+  $scope.attemptedLastName = "not yet set";
   $scope.attemptedUsername = "not yet set";
   $scope.attemptedPassword = "not yet set";
 
@@ -30,4 +32,31 @@ angular.module('app.auth', [])
       }
       );
   };
+
+  $scope.attemptSignup = function () {
+    $scope.showUnauthMsg = false;
+    $http.post('/api/signup', {
+      firstName: $scope.attemptedFirstName,
+      lastName: $scope.attemptedLastName,
+      username: $scope.attemptedUsername,
+      password: $scope.attemptedPassword
+    })
+    .then(
+      //success callback
+      function(res){
+        console.log('sent login credentials');
+        console.log('res.data', res.data);
+        res.redirect('/#/dashboard?username=' + res.body.username);
+      },
+      //error callback
+      function(res){
+        console.log('res:', res);
+        if(res.status === 401){
+          console.log('unauthorized credentials');
+          $scope.showUnauthMsg = true;
+        }
+      }
+      );
+  };
+
 }]);
